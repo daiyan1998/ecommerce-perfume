cc1036453a4a20aca04a65733cf44b7f56dd94ec
daiyan1998 <abduddaiyan7@gmail.com>
2023-11-11 01:11:14 +0600

    added user router in backend

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} from "../controllers/userController";
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser);
