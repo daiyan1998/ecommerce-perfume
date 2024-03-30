9856e2f71f0558ad2a7aa054a1334b060ceba5ae
daiyan1998 <abduddaiyan7@gmail.com>
2024-03-30 07:40:29 +0600

    updated package.json script

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const API_URL = "https://ecommerce-sabbir.vercel.app/";
+const API_URL = process.env.API_URL;
 const nextConfig = {
   images: {
     remotePatterns: [
