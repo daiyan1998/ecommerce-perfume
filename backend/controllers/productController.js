11604126958904b92c34032d8614f38607ce01e3
daiyan1998 <abduddaiyan7@gmail.com>
2024-08-30 01:00:21 +0600

    feat(server): add product creation and update functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,46 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts };
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Product",
+    user: req.user._id,
+    image: "/images/sample.png",
+    category: "Sample category",
+    brand: "Sample Brand",
+    description: "This is a sample product description.",
+    priceByMl: [
+      { ml: 3, price: 100 },
+      { ml: 6, price: 180 },
+      { ml: 12, price: 380 },
+    ],
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, priceByMl, description, image, brand, category } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.priceByMl = priceByMl;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+export { getProductById, getProducts, createProduct, updateProduct };
